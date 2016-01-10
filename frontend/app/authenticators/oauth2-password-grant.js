368ea0bbcea86aeac0e08916653a51794b8290b8
Lars Bünting <l.buenting@e-patrol.com>
2016-01-10 21:13:57 +0100

    fixed token auth authenticator

--- frontend/app/authenticators/oauth2-password-grant.js
+++ frontend/app/authenticators/oauth2-password-grant.js
@@ -1,8 +1,33 @@
 // app/authenticators/oauth2.js
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
+import Ember from 'ember';
 
 export default
 OAuth2PasswordGrant.extend({
     serverTokenEndpoint: '/api/v1/api-token-auth/',
-    serverTokenRevocationEndpoint: '/api/v1/api-token-auth/'
+    serverTokenRevocationEndpoint: '/api/v1/api-token-auth/',
+
+    restore: function restore(data) {
+        var _this = this;
+
+        return new Ember.RSVP.Promise(function (resolve, reject) {
+            var now = new Date().getTime();
+            var refreshAccessTokens = _this.get('refreshAccessTokens');
+            if (!Ember.isEmpty(data['expires_at']) && data['expires_at'] < now) {
+                if (refreshAccessTokens) {
+                    _this._refreshAccessToken(data['expires_in'], data['refresh_token']).then(resolve, reject);
+                } else {
+                    reject();
+                }
+            } else {
+                if (Ember.isEmpty(data['token'])) {
+                    reject();
+                } else {
+                    _this._scheduleAccessTokenRefresh(data['expires_in'], data['expires_at'], data['refresh_token']);
+                    resolve(data);
+                }
+            }
+        });
+    }
+
 });
--- frontend/app/authorizers/oauth2-bearer.js
+++ frontend/app/authorizers/oauth2-bearer.js
@@ -2,9 +2,16 @@
  * Created by larsbunting on 05.12.15.
  */
 import OAuth2Bearer from 'ember-simple-auth/authorizers/oauth2-bearer';
+import Ember from 'ember';
 
 export default
 OAuth2Bearer.extend({
-    identificationAttributeName: 'Token'
+    identificationAttributeName: 'Token',
+    authorize(data, block) {
+        const accessToken = data['token'];
+        if (!Ember.isEmpty(accessToken)) {
+            block('Authorization', `Bearer ${accessToken}`);
+        }
+    }
 
 });
