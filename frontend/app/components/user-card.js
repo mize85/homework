fa196a18e0b44fab0224ece95c1e561433dee82b
Lars Bünting <lars.buenting@smacc.io>
2017-06-09 18:27:34 +0200

    random image fixed

--- frontend/app/components/user-card.js
+++ frontend/app/components/user-card.js
@@ -4,6 +4,6 @@ const {Component, computed} = Ember;
 
 export default Component.extend({
   randomImage: computed('model.id', function() {
-    return `http://lorempixel.com/output/people-q-g-640-480-${this.get("model.id")}.jpg`;
+    return `http://lorempixel.com/output/people-q-c-640-480-${this.get("model.id")}.jpg`;
   })
 });
